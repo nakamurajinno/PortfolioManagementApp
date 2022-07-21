9557ad9afadd43d9f7a0a0a369649b03703436e3
nagata-ginga <105192573+nagata-ginga@users.noreply.github.com>
2022-07-21 16:13:31 +0900

    永田、コミット

--- app/src/main/java/jp/co/trustsystem/portfoliomanagementapp/MainActivity.java
+++ app/src/main/java/jp/co/trustsystem/portfoliomanagementapp/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(tabs, pager,
                 (tab, position) -> tab.setText(tabTitleArray[position])
         ).attach();
-
     }
 
 
